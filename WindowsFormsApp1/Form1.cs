68815380658ce5a02b587237e04667a6cf6e0052
Richard Z Troester <rzachprime@gmail.com>
2017-10-19 00:43:16 -0400

    Created a simple windows form and added a button to make a message box appear with the message Hello!

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,22 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SayHello();
+
+          
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show("Hello!!");
+        }
     }
 }
